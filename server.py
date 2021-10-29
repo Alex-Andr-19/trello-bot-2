6024e25da31dc0c84f17dd8563540f5fa1bf6123
Andrew <andrey.aleksandrychev2@mail.ru>
2021-10-29 04:13:38 +0300

    it s alive

--- server.py
+++ server.py
@@ -5,7 +5,13 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST','HEAD'])
 def webhook():
     if request.method == 'POST':
-        print(request.json)
+        print(json.dumps(
+            request.json,
+            sort_keys=True, 
+            indent=4, 
+            separators=(",", ": "), 
+            ensure_ascii=False)
+        )
         return 'success', 200
     elif request.method == 'HEAD':
         print('connect')
