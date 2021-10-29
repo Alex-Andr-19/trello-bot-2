d79fa52c2b570a0ff3ec55d18d3276903b67e6f2
Andrew <andrey.aleksandrychev2@mail.ru>
2021-10-29 03:22:01 +0300

    New commit 1

--- server.py
+++ server.py
@@ -2,13 +2,13 @@ from flask import Flask, request, abort
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/webhook', methods=['POST','GET'])
 def webhook():
     if request.method == 'POST':
         print(request.json)
         return 'success', 200
-    else:
-        abort(400)
-
+    elif request.method == 'GET':
+        print('connect')
+        return 'success', 200
 if __name__ == '__main__':
     app.run()
